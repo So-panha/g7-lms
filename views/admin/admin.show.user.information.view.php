1c32f0c72120f16a440a42877fdee4419297768b
Kemleanglor <kemleang.lor@student.passerellesnumeriques.org>
2024-02-17 17:00:11 +0700

    [feat] form show information user GLS-48

---
+++ views/admin/admin.show.user.information.view.php
@@ -0,0 +1,41 @@
+<!-- form edit -->
+<div class="col-xl-9 col-lg-8  col-md-12 grow ">
+    <div class="d-flex ">
+        <div class="col" style="background-color: #ACABCC; text-align: center;">
+            <img src="assets/images/profiles/img-2.jpg" alt="Lights" style="width:60%; height:40%; border-radius: 50%;" class="rounded-circle mt-5">
+            <div class="caption mt-4  Font-weight: bold " style="color: black;">
+                <h3>JIMIN PARK</h3>
+            </div>
+        </div>
+        <div class="col-7" style="background-color:whitesmoke;">
+            <div class="col mt-4">
+                <p>First Name:</p>
+                <p></p>
+                <hr>
+            </div>
+            <div class="col mt-5">
+                <p>Last Name:</p>
+                <p></p>
+                <hr>
+            </div>
+            <div class="col mt-5">
+                <p>Position:</p>
+                <p></p>
+                <hr>
+            </div>
+            <div class="col mt-5">
+                <p>Province:</p>
+                <p></p>
+                <hr>
+            </div>
+            <div class="col mt-5">
+                <p>Salary:</p>
+                <p></p>
+                <hr>
+            </div>
+            <div class="d-flex justify-content-end pb-3 mr-4;">
+                <button type="button" class="btn btn-danger mr-3" style="width:80px;">Edit</button>
+            </div>
+        </div>
+    </div>
+</div>
