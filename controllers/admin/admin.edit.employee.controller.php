651e297e193be9ca3cc5d27dd8b239845c5ad406
RinYouern <rin.youern@student.passerellesnumeriques.org>
2024-02-23 09:29:57 +0700

    merge code

---
+++ controllers/admin/admin.edit.employee.controller.php
@@ -0,0 +1,7 @@
+<?php
+require 'database/database.php';
+require 'models/admin.model.php';
+$user_id = $_GET['id'] ? $_GET['id'] : null;
+$users = getUser($user_id);
+
+require "views/admin/admin.edit.employee.view.php";
