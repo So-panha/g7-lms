2059ff5eaf166a6a9c16d5822d9f6a87d50fdd00
Kemleanglor <kemleang.lor@student.passerellesnumeriques.org>
2024-03-01 10:52:32 +0700

    [bug] fix_active_navbar GLS-153

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -78,7 +78,7 @@ if (!empty($_SESSION['user'])) {
                                     </div>
                                     <?php if ($_SESSION['user']['role'] != 'employee') : ?>
                                         <div class="col-6 align-items-center shadow-none text-center">
-                                            <a href="<?=$link?>" class="text-dark p-4 second-slider-btn ctm-border-right ctm-border-top <?php if ( $_SERVER['REQUEST_URI'] == '/information_user' || $_SERVER['REQUEST_URI'] == '/add_employee'|| $_SERVER['REQUEST_URI'] == '/members' ) echo 'active'; ?>"><span class="lnr lnr-users pr-0 pb-lg-2 font-23"></span><span class="">Employees</span></a>
+                                            <a href="<?=$link?>" class="text-dark p-4 second-slider-btn ctm-border-right ctm-border-top <?php if ( $_SERVER['REQUEST_URI'] == '/information_user' || $_SERVER['REQUEST_URI'] == '/add_employee'|| $_SERVER['REQUEST_URI'] == '/members'|| $_SERVER['REQUEST_URI'] == '/admin_employees' ) echo 'active'; ?>"><span class="lnr lnr-users pr-0 pb-lg-2 font-23"></span><span class="">Employees</span></a>
                                         </div>
                                     <?php endif; ?>
                                     <div class="col-6 align-items-center shadow-none text-center">
