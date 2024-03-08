cef9f94471bff502655cf1ac4bd1f45ecaede72b
RinYouern <rin.youern@student.passerellesnumeriques.org>
2024-03-08 10:41:05 +0700

    [feat] show list of employee leave perday on manager and employee GLS-159

--- controllers/employees/manager/manager.controller.php
+++ controllers/employees/manager/manager.controller.php
@@ -1,2 +1,8 @@
 <?php
+require "database/database.php";
+require "models/admin.model.php";
+// call function from database
+
+// Get all type leave
+$typeLeave = typeLeaves();
 require 'views/employees/manager/manager.view.php';
