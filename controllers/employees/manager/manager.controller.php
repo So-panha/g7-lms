1863fdd8c8925c74a0b3a1925eb4cbd7889e90ad
RinYouern <rin.youern@student.passerellesnumeriques.org>
2024-03-14 08:10:07 +0700

    [feat] show list of employee leave next day

--- controllers/employees/manager/manager.controller.php
+++ controllers/employees/manager/manager.controller.php
@@ -7,4 +7,9 @@ $users = getUsers();
 $manager = managers();
 // Get all type leave
 $typeLeave = typeLeaves();
+//id namaager
+$manager_id = $_SESSION['user']['user_id'] ;
+//get all employee request
+$allRequests = memberRequest($manager_id);
+
 require 'views/employees/manager/manager.view.php';
