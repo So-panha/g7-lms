62902c60f5546e14f01ea784f2f3b301d61cc14f
SOPANHA.SIN <SOPANHA.SIN@PNC.LAN>
2024-03-18 15:31:24 +0700

    [bug] Fix_leave_request GLS-198

--- controllers/leaves/create_request_leave.php
+++ controllers/leaves/create_request_leave.php
@@ -4,49 +4,62 @@ require '../../database/database.php';
 require '../../models/employee.model.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retrieve the values from the form
-    $leaveType = $_POST['leaveType'];
-    $from = $_POST['from'];
-    $to = $_POST['to'];
-    $halfDay = $_POST['halfDay'];
-    $reason = $_POST['reason'];
-    $checked = "Pending";
-    $date_request = $_POST['date'];
-    $user_id = $_POST['user_id'];
-    $day = $_POST['day'];
+    if (
+        !empty($_POST['leaveType'])
+        && !empty($_POST['from'])
+        && !empty($_POST['to'])
+        && !empty($_POST['date'])
+        && !empty($_POST['user_id'])
+        // && !empty($_POST['day'])
+    ) {
+        // Retrieve the values from the form
+        $leaveType = $_POST['leaveType'];
+        $from = $_POST['from'];
+        $to = $_POST['to'];
+        $halfDay = $_POST['halfDay'];
+        $reason = $_POST['reason'];
+        $checked = "Pending";
+        $date_request = $_POST['date'];
+        $user_id = $_POST['user_id'];
+        $day = $_POST['day'];
 
-    // Convert dates to DateTime objects
-    $dateFrom = DateTime::createFromFormat('d/m/Y', $from);
-    $dateTo = DateTime::createFromFormat('d/m/Y', $to);
 
-    // Get the timestamps from DateTime objects
-    $timestampFrom = $dateFrom->getTimestamp();
-    $timestampTo = $dateTo->getTimestamp();
+        // Convert dates to DateTime objects
+        $dateFrom = DateTime::createFromFormat('d/m/Y', $from);
+        $dateTo = DateTime::createFromFormat('d/m/Y', $to);
 
-    // Calculate the difference in days
-    $dayDifference = floor(($timestampTo - $timestampFrom) / (60 * 60 * 24));
-    $dayRemind = $day - $dayDifference;
-    //taken
-    $taken += $dayDifference;
-    
-    // Check if the 'to' date is earlier than the 'from' date
-    if ($timestampTo < $timestampFrom) {
-        //session alert for request leave error 
-        $_SESSION['leave'] = 'Unsuccess'; 
-        header('Location: /leaves');
-    } else {
+        // Get the timestamps from DateTime objects
+        $timestampFrom = $dateFrom->getTimestamp();
+        $timestampTo = $dateTo->getTimestamp();
 
-        // Call the insertLeaveRequest function
-        $insert = insertLeaveRequest($leaveType, $from, $to, $checked, $reason, $date_request, $user_id);
-        $days = days($dayRemind,$user_id);
-        $taken = taken($taken,$user_id);
+        // Calculate the difference in days
+        $dayDifference = floor(($timestampTo - $timestampFrom) / (60 * 60 * 24));
+        $dayRemind = $day - $dayDifference;
+        //taken
+        $taken += $dayDifference;
 
-        // Check if the leave request was successfully inserted
-        if ($insert) {
-            // session alert when request successfully
-            $_SESSION['leave'] = 'Success';
+        // Check if the 'to' date is earlier than the 'from' date
+        if ($timestampTo < $timestampFrom) {
+            //session alert for request leave error 
+            $_SESSION['leave'] = 'Unsuccess';
             header('Location: /leaves');
-            exit;
+        } else {
+
+            // Call the insertLeaveRequest function
+            $insert = insertLeaveRequest($leaveType, $from, $to, $checked, $reason, $date_request, $user_id);
+            $days = days($dayRemind, $user_id);
+            $taken = taken($taken, $user_id);
+
+            // Check if the leave request was successfully inserted
+            if ($insert) {
+                // session alert when request successfully
+                $_SESSION['leave'] = 'Success';
+                header('Location: /leaves');
+                exit;
+            }
         }
+    } else {
+        $_SESSION['leave'] = 'Unsuccess';
+        header('Location: /leaves');
     }
 }
