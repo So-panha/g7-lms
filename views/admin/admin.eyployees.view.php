fffe78de7816662d2d6f483fe19ae0a4f80581f2
SOPANHA.SIN <SOPANHA.SIN@PNC.LAN>
2024-03-20 20:31:58 +0700

    [feat] search_name_and_select_role GLS-202

--- views/admin/admin.eyployees.view.php
+++ views/admin/admin.eyployees.view.php
@@ -29,6 +29,20 @@
             <!-- <li class="list-group-item text-center button-5"><a href="" class="text-white">All</a></li> -->
             <li class="list-group-item text-center button-5 active"><a href="" class="text-white">All</a></li>
             <li class="list-group-item text-center button-6"><a href="/admin_employees_team" class="text-dark">Add Team</a></li>
+
+            <form action="" style="position: absolute;right:30%;">
+                <select class="form-control me-2" id="select-role">
+                    <option disabled selected>Employees Roles</option>
+                    <option value="">All</option>
+                    <option value="Manager">Manager</option>
+                    <option value="Employee">Employees</option>
+                </select>
+            </form>
+
+            <form class="d-flex" style="position: absolute;right: 1.5%;">
+                <input class="form-control me-2" type="text" placeholder="Search" id="search-employees">
+                <button class="btn btn-warning" type="button">Search</button>
+            </form>
         </ul>
     </div>
     <!-- header -->
@@ -69,7 +83,7 @@
                 }
             }
         ?>
-            <div class="user-card card shadow-sm bg-white text-center ctm-border-radius grow mr-2 ml-2 mt-4">
+            <div class="user-card card shadow-sm bg-white text-center ctm-border-radius mr-2 ml-2 mt-4" id="main_card_user">
                 <a href="/information_user?id=<?php echo urlencode($id); ?>">
                     <div class="user-info card-body" style="width:260px;">
                         <div class="user-avatar mb-4">
@@ -81,10 +95,9 @@
                         </div>
                         <div class="user-details">
                             <h5><b><?php echo strtoupper($name); ?></b></h5>
+                            <input hidden type="text" value="<?= $user['role']; ?>">
                             <p><?php echo $positionName; ?></p>
                             <input type="hidden" value="<?= $id ?>">
-
-
                         </div>
                     </div>
 
@@ -96,6 +109,33 @@
         ?>
     </div>
 </div>
+<!-- Script search bar and select -->
+<script>
+    var users = document.querySelectorAll("#main_card_user");
+    document.getElementById('search-employees').addEventListener('input', (e) => {
+        users.forEach(user => {
+            var name = document.getElementById('search-employees').value.toLocaleLowerCase();
+            let userName = user.children[0].children[0].children[1].children[0].children[0].textContent.toLowerCase();
+            if (userName.includes(name) === true) {
+                user.style.display = 'block';
+            } else {
+                user.style.display = 'none';
+            }
+        });
+    });
+    
+    document.getElementById('select-role').addEventListener('change',(e)=>{
+        users.forEach(user => {
+            var roles = document.getElementById('select-role').value.toLocaleLowerCase();
+            let userName = user.children[0].children[0].children[1].children[1].value.toLocaleLowerCase();
+            if (userName.includes(roles) === true) {
+                user.style.display = 'block';
+            } else {
+                user.style.display = 'none';
+            }
+        });
+    });
+</script>
 <!--  -->
 </div>
 </div>
