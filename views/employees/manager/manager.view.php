7ef7856e8174118895cf71e9bd90f8f51976b016
RinYouern <rin.youern@student.passerellesnumeriques.org>
2024-03-21 15:47:26 +0700

    [ref] add code show leave next day GLS-159

--- views/employees/manager/manager.view.php
+++ views/employees/manager/manager.view.php
@@ -21,7 +21,7 @@
         <div class="card-body">
             <ul class="list-group list-group-horizontal-lg">
                 <li class="list-group-item text-center active button-5">
-                    <p class="text-white" >Manager Dashboard</p>
+                    <p class="text-white">Manager Dashboard</p>
                 </li>
             </ul>
         </div>
@@ -72,11 +72,11 @@
                 <div class="card-body text-center">
                     <div class="time-list">
                         <div class="dash-stats-list">
-                            <span class="btn btn-outline-primary"><?php echo $taken ." Days" ?></span>
+                            <span class="btn btn-outline-primary"><?php echo $taken . " Days" ?></span>
                             <p class="mb-0">Taken</p>
                         </div>
                         <div class="dash-stats-list">
-                            <span class="btn btn-outline-primary"><?php echo $dayCanLeave ." Days" ?></span>
+                            <span class="btn btn-outline-primary"><?php echo $dayCanLeave . " Days" ?></span>
                             <p class="mb-0">Remaining</p>
                         </div>
                     </div>
@@ -122,7 +122,8 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-6 col-md-12 d-flex">
+
+        <div class="col-lg-6 d-flex">
             <!-- Team Leads List -->
             <div class="card flex-fill team-lead shadow-sm grow">
                 <div class="card-header">
@@ -139,6 +140,51 @@
                         </div>
                         <hr>
                     <?php endfor; ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-12 col-md-12 d-flex">
+            <!-- Today -->
+            <div class="card flex-fill today-list shadow-sm grow">
+                <div class="card-header">
+                    <h4 class="card-title mb-0 d-inline-block">
+                        Date Leave
+                    </h4>
+                    <a href="leave.html" class="d-inline-block float-right text-primary"><i class="fa fa-suitcase"></i></a>
+                </div>
+                <div class="card-body recent-activ">
+                    <div class="recent-comment">
+                        <?php
+                        foreach ($allRequests as $request) {
+
+                            if ($request['start_leave'] > date('d/m/Y')) {
+                        ?>
+                                <a href="javascript:void(0)" class="dash-card text-danger">
+                                    <div class="dash-card-container">
+                                        <div class="dash-card-content mr-2">
+                                            <img src="assets/images/profiles/<?= $request['picture'] ?>"" alt=" User Avatar" class=" rounded-circle" width="40px" height="40px">
+                                        </div>
+                                        <div class="dash-card-content mr-2">
+                                            <h6 class="mb-0"><?php echo strtoupper($request['fname']) ?></h6>
+                                        </div>
+                                        <div class="dash-card-content">
+                                            <h6 class="mb-0"><?php echo "will leave on " . $request['start_leave'] . " and back on " . $request['end_leave'] . "."?></h6>
+                                        </div>
+                                        <div class="dash-card-content">
+                                            <h6 class="mb-0 ml-1"><?php echo " Type leave: " . $request['type_leave_name'] ?></h6>
+                                        </div>
+                                    </div>
+                                </a>
+                                <hr />
+                        <?php
+                            }
+                        }
+                        ?>
+
+
+
+                    </div>
                 </div>
             </div>
         </div>
